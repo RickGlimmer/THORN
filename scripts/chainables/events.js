d3d1765ed3689ff7f4c16fc558f54d8c14b9b8c6
RickGlimmer <github@microblocks.asia>
2015-02-02 22:37:27 +0000

    cleaned up syntax added bubbles and cancelable to fire function

--- scripts/chainables/events.js
+++ scripts/chainables/events.js
@@ -1,54 +1,45 @@
 ﻿þ.extend('CHAINABLES', {
     bind: function (type, fn, capture) {
-        var modtype = type;
-        var types = modtype.split(" ");
+        var i, types = type.split(" ");
+
         return this.each(function () {
             for (i = 0; i < types.length; i++)
                 this.addEventListener(types[i], fn, capture ? true : false);
         }, true);
-    }
-    ,
+    },
+
     unbind: function (type, fn, capture) {
-        var modtype = type;
-        var types = modtype.split(" ");
+        var i, types = types.split(" ");
+
         return this.each(function () {
             for (i = 0; i < types.length; i++)
                 this.removeEventListener(types[i], fn, capture ? true : false);
         }, true);
-    }
-    ,
-    fire: function (type, detail) {
-        return this.each(function () {
-            var me = this;
-            if (me == document && !me.dispatchEvent)
-                me = document.documentElement;
+    },
 
+    fire: function (type, detail, canbubblearg, cancelablearg) {
+        return this.each(function () {
+            var event;
+            var elem = typeof this == 'document' ? document.documentElement : this;
+            var canBubble = typeof canbubblearg == 'undefined' ? true : canbubblearg;
+            var cancelable = typeof cancelablearg == 'undefined' ? true : cancelablearg;
 
             if (typeof window.Event == "function") {
-                var event = new Event(type, {
-                    'bubbles': true,
-                    'cancelable': true
+                event = new Event(type, {
+                    'bubbles': canBubble,
+                    'cancelable': cancelable
                 });
                 event.detail = detail;
-                me.dispatchEvent(event);
+                elem.dispatchEvent(event);
             } else {
-                var event = document.createEvent('HTMLEvents');
-                event.initEvent(type, true, true);
+                event = document.createEvent('HTMLEvents');
+                event.initEvent(type, canBubble, cancelable);
                 event.detail = detail;
                 event.eventName = type;
-                me.dispatchEvent(event);
+                elem.dispatchEvent(event);
             }
 
         }, true);
     }
-    /*        ,
-            message: function (type, data) {
-                return this.each(function () {
-                    var subscribers = $(this).attr("data-subscribers");
-                    if (subscribers != null) {
-                        þ(subscribers).fire(type, data);
-                    }
-                });
-            },
-    */
+
 });
