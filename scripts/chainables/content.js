edb7d72f9f2937c6cb11c1904b50d997957b295e
RickGlimmer <github@microblocks.asia>
2015-02-02 22:35:49 +0000

    Extract Plugin calls and remove redundant out commented source

--- scripts/chainables/content.js
+++ scripts/chainables/content.js
@@ -3,76 +3,37 @@
         this.each(function () {
             if (this.parentNode)
                 this.parentNode.removeChild(this);
-        });
+        }, true);
     },
-/*
-    attr: function (attribute, val) {
-        if (arguments.length == 2) {
-            if (val == "false") val = false;
-            if (val == "true") val = true;
-            return this.each(function () {
-                if ((attribute == 'checked' || attribute == "disabled") && (val == '' || val == false || typeof val == "undefined")) {
-                    this.removeAttribute(attribute)
-                }
-                else {
-                    if (this.getAttribute(attribute) != val) {
-                        this.setAttribute(attribute, val);
-                    }
-                }
-            });
-        } else {
-            var attrs = [];
-            this.each(function () {
-                var val = this.getAttribute(attribute);
-                if (val != null)
-                    attrs.push(val);
-            });
-            return attrs.length == 0 ? null : attrs.length == 1 ? attrs[0] : attrs;
-        }
-    },
-*/
+
     html: function (value) {
         if (value === undefined) return this.innerHTML;
         this.each(function () {
-            this.innerHTML = value;//innerHTML on the þ node shortcut for this.nodes[i].innerHTML
+            this.innerHTML = value;//innerHTML on the þ object NOT on the DOM node
         });
-        if (value.indexOf("[data-plugin]") > 0) {
-            þ.attachPlugins(this);
-        }
         return this;
-    }
-/*    ,
+    },
+    /*
+    TODO.Add a xhr (XMLHTTPRequest) extension to dynamically load content    
+
     load: function (url, onSuccess, onFailure) {
-        if (this[0]) {
-            if (this[0].innerHTML === undefined)
-                return this;
-            else {
-                THORN.xhr(
-                    url,
-                    { async: true, method: 'GET', context: this[0] },
-                    function (req, context) {
-                        context.innerHTML = req.responseText;
-                        $.attachPlugins(context);
-                        if (typeof onSuccess == 'function')
-                            onSuccess(context, req);
-                    },
-                    function (req, context) {
-                        if (typeof onFailure == 'function')
-                            onFailure(context, req);
-                    }
-                );
-            }
+        var context = this;
+        if (context.innerHTML !== undefined) {
+            THORN.xhr(
+                url,
+                { async: true, method: 'GET', context: context },
+                function (req, context) {
+                    context.innerHTML = req.responseText;
+                    if (typeof onSuccess == 'function')
+                        onSuccess(context, req);
+                },
+                function (req, context) {
+                    if (typeof onFailure == 'function')
+                        onFailure(context, req);
+                }
+            );
         }
         return this;
     }
 */
 });
-
-
-
-
-/*    ,
-        isCheckbox: function () {
-                    return THORN.nodeName(this[0], "input") && this[0].type == "checkbox";
-                },
-        */
