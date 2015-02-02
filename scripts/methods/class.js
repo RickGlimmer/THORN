cfe5d11e5a0f9193427707210e6fb86d0afab7eb
RickGlimmer <github@microblocks.asia>
2015-02-02 18:09:55 +0000

    use CHAINABLEMETHODS instead of METHODS for methods that return 'this'

--- scripts/methods/class.js
+++ scripts/methods/class.js
@@ -1,4 +1,4 @@
-﻿þ.extend('METHODS', {
+﻿þ.extend('CHAINABLEMETHODS', {
     hasClass: function (className, index) {
         //Take the node, split classname, lookup in resulting array
         return this.nodes[index?index:0].className.split(' ').indexOf(className) != -1
