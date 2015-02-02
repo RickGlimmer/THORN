5eb02dc44b7a2f009e6daad7ee558c9ed3491453
RickGlimmer <github@microblocks.asia>
2015-02-02 22:04:20 +0000

    Reorganise order, clean up syntax

--- scripts/properties/PROPERTIES.js
+++ scripts/properties/PROPERTIES.js
@@ -1,18 +1,14 @@
 ﻿þ.extend('PROPERTIES', {
-    //This can not be chained as its use is þelem.html = "hello" or var t = þelem.html;
-    innerHTML: {
-        configurable: false,
-        set: function (value) {
-            this.each(
-                    function () {
-                        this.innerHTML = value;
-                    }, true)
-        },
-        get: function () {
-            return this.nodes[0].innerHTML;
-        }
-    },
+    //These can not be chained as all these properties return a value
+    id: { get: function () { return this.nodes[0].id } },
+    dataset: { get: function () { return this.nodes[0].dataset; } },
+
+    //Access to the raw nodes
+    length: { get: function () { return this.nodes ? this.nodes.length : 0; } },
     firstNode: { get: function () { return this.nodes[0] } },
+    allNodes: { get: function () { return this.nodes; } },
+
+    //Width,height and position of the first node
     clientWidth: { get: function () { return this.nodes[0].clientWidth; } },
     offsetWidth: { get: function () { return this.nodes[0].offsetWidth; } },
     scrollWidth: { get: function () { return this.nodes[0].scrollWidth; } },
@@ -23,83 +19,69 @@
     //Position relative to its container
     offsetTop: { get: function () { return this.nodes[0].offsetTop; } },
     offsetLeft: { get: function () { return this.nodes[0].offsetLeft; } },
-    offset: {
-        get: function () {
-            return { left: this.nodes[0].offsetLeft, top: this.nodes[0].offsetTop }
-        }
-    },
 
     //Position relative to document
     position: {
         get: function () {
             var node = this.nodes[0],
-             x = parseInt(node.offsetLeft),
-             y = parseInt(node.offsetTop);
+             left = parseInt(node.offsetLeft),
+             top = parseInt(node.offsetTop);
 
             node = node.offsetParent;
+
             while (node != null) {
-                x += parseInt(node.offsetLeft);
-                y += parseInt(node.offsetTop);
+                left += parseInt(node.offsetLeft);
+                top += parseInt(node.offsetTop);
                 node = node.offsetParent;
             }
-            return { left: x, top: y };
+            return { left: left, top: top };
         }
     },
 
-    id: { get: function () { return this.nodes[0].id } },
-    dataset: { get: function () { return this.nodes[0].dataset; } },
-
+    //Read or write the innerHTML of an node
+    innerHTML: {
+        set: function (value) {
+            this.each(
+                    function () {
+                        if (this.innerHTML) // Check if this node supports innerHTML
+                            this.innerHTML = value;
+                    }, true)
+        },
+        get: function () {
+            return this.nodes[0].innerHTML;
+        }
+    },
 
     value: {
         get: function () {
             var node = this.nodes[0];
-            if (node.nodeType == 1) {//node is an element
+            if (node.nodeType == 1) { //node is an element
                 switch (node.tagName) {
                     case 'INPUT':
                         if (node.type == 'checkbox') return node.checked;
                         break;
                     case 'OPTION':
                         return node.value || node.text;
-                        break;
-                        // Handle select boxes special
                     case 'SELECT':
-                        var index = node.selectedIndex,
-                        values = [],
-                        options = node.options,
-                        one = node.type == "select-one";
+                        var i, max, index = node.selectedIndex, values = [],
+                        options = node.options, one = node.type == "select-one";
 
-                        // Nothing was selected
-                        if (index < 0) return null;
+                        if (index < 0) return null; // Nothing was selected
 
-                        // Loop through all the selected options
-                        for (var i = one ? index : 0, max = one ? index + 1 : options.length; i < max; i++) {
+                        if (one) return options[index].value || options[index].text;
+
+                        // Loop through all options and find the selected ones
+                        for (i = 0, max = options.length; i < max; i++) {
                             var option = options[i];
-                            if (option.selected) {
-                                // Get the specifc value for the option
-                                value = option.value || option.text;
-
-                                // We don't need an array for one selects
-                                if (one)
-                                    return value;
-
-                                // Multi-Selects return an array
-                                values.push(value);
-                            }
+                            if (option.selected) values.push(option.value || option.text);
                         }
                         return values;
                 }
             }
-            // Everything else, we just grab the value
+            // Everything else
             return (node.value || "").replace(/\r/g, "");
         }
     },
 
-    encodedValue: function () {
-        return window.encodeURIComponent(this.value);
-    },
-
-    //Number of nodes
-    length: {
-        get: function () { return this.nodes ? this.nodes.length : 0; }
-    }
+    encodedValue: { get: function () { return window.encodeURIComponent(this.value); } }
 });
