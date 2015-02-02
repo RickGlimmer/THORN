52520d743ee8796e5a8bf6f6c42808201311ed90
RickGlimmer <github@microblocks.asia>
2015-02-02 21:39:16 +0000

    clean up the syntax and add support for sealed extensions

--- scripts/THORN.js
+++ scripts/THORN.js
@@ -1,11 +1,11 @@
 ﻿/*
  þ THORN: micro FrameWork (jQuery style syntax)
  Author: Rick Glimmer
- Date: 2015-01-28
+ Date: 2015-02-02 21:15+07:00
  Version: 0.1
 */
 "use strict";
-!(function () {
+(function () {
     //Objects to hold all added functionalities
     var _Chainables = Object.create(null);
     var _Properties = Object.create(null);
@@ -13,14 +13,14 @@
     //This is the global selector function
     var THORN = function (query, context) {
         return query ? getNodes(query, context) : null;     // Return a list of nodes 
-    }
+    };
     
     //internal function to select all nodes in the DOM that satifies the parameters
     //Return value will have a nodes list with all the raw DOM nodes
     //.length can be used how many nodes are found
     //_Chainable functions use the nodes and allow chaining.
     //_Properties can get or set a value on a the first node or when possible on all nodes.
-    function getNodes (selector, context) {
+    var getNodes = function (selector, context) {
         var i, l, nodeType;
         var root = context || document;
         var o = Object.create(_Chainables, _Properties);
@@ -44,8 +44,8 @@
     }
 
     //This allows to extend THORN with extra functions and attributes
-    THORN.extend = function (targetname, obj) {						// Merge to objects
-        var prop, target;
+    THORN.extend = function (targetname, obj) {
+        var prop, target, sealed;
         switch (targetname) {
             case 'CHAINABLEMETHODS': //Falls through as there is no separate methods object
             case 'CHAINABLES': target = _Chainables; break;
@@ -57,14 +57,16 @@
         }
         for (prop in obj) {
             if (obj.hasOwnProperty(prop)) {
-                target[prop] = obj[prop];
+                sealed = target[prop] && target[prop].sealed;
+                if (!sealed)
+                    target[prop] = obj[prop];
             }
         }
         return target;
     };
 
     //THORN collects all onready functions
-    var onLoadFunctions = []
+    var onLoadFunctions = [];
 
     //First attaches all Plugins to their nodes and then calls the
     //registered onready functions one by one.
@@ -81,7 +83,7 @@
     THORN.onready = function (fn) {
         //register the onready function
         onLoadFunctions.push(fn);
-    }
+    };
 
     document.addEventListener('DOMContentLoaded', DOMReady, false);
 
